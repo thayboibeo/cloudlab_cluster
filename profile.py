99959ab19f3e1fd07e0767abcc0356ee8100900c
thayboibeo <43081357+thayboibeo@users.noreply.github.com>
2018-09-08 07:16:45 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -62,7 +62,8 @@ for i in range(6):
   iface.addAddress(pg.IPv4Address("192.168.1." + str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
   
-  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/passwordless.sh"))
+  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/passwordless.sh"))
+  node.addService(pg.Execute(shell="sh", command="sudo . /local/repository/passwordless.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
