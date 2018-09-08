9af6cd28cf0313f546a148dccaa4a67f882ca92d
thayboibeo <43081357+thayboibeo@users.noreply.github.com>
2018-09-07 20:43:12 -0400

    Create profile.py

---
+++ profile.py
@@ -0,0 +1,21 @@
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+
+# Create a portal context.
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+ 
+# Add a raw PC to the request.
+node = request.XenVM("node")
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+node.routable_control_ip = "true"
+
+# Install and execute a script that is contained in the repository.
+node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+
+# Print the RSpec to the enclosing page.
+pc.printRequestRSpec(request)
