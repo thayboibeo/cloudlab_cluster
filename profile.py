295aa527585e1c0900cc6f9a3a090782c25804bb
thayboibeo <43081357+thayboibeo@users.noreply.github.com>
2018-09-17 22:25:48 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -64,13 +64,11 @@ for i in range(6):
   
   node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/passwordless.sh"))
   node.addService(pg.Execute(shell="sh", command="sudo /local/repository/passwordless.sh"))
-  
-  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
-  
+    
   if i == 0:
+    node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
     node.addService(pg.Execute(shell="sh", command="cp /local/repository/source/* /users/lngo"))
   
-
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
