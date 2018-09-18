9ab65c73646198b9a3d23acb20e6af81724c1baa
thayboibeo <43081357+thayboibeo@users.noreply.github.com>
2018-09-17 21:00:36 -0400

    Update install_mpi.sh

--- install_mpi.sh
+++ install_mpi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 sudo yum -y group install "Development Tools"
 sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
 sudo tar xzf openmpi-3.1.2.tar.gz
